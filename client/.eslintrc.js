bc9ac62cd1ccc998264d144345b08943cec53a64
tim <timon0596@yandex.ru>
2021-03-18 23:46:34 +0300

    1

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,24 +1,33 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      plugins: ['@typescript-eslint'],
+      // rules: {
+      //   'no-use-before-define': 'off',
+      //   '@typescript-eslint/no-use-before-define': ['error'],
+      // },
     },
-    "extends": [
-        "plugin:react/recommended",
-        "airbnb"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+  },
 };
--- server/app.js
+++ server/app.js
@@ -1,11 +1,55 @@
 const WebSocketServer = require("ws").Server;
     const express = require("express");
     const http = require("http");
+    const Sequelize = require('sequelize');
+    const cors = require('cors')
+    const bodyParser = require('body-parser')
 
     const app = express();
+    app.use(cors())
+    app.use(bodyParser.urlencoded({
+      extended: true
+    }));
+    app.use(bodyParser.json());
     const server = http.createServer(app);
 
     server.listen(8080);
+
+
+
+
+
+    
+
+    const sequelize = new Sequelize('socket', 'root', 'root',{
+      host: 'localhost',
+      dialect: 'mysql',
+      port: '3300'
+    });
+    
+    const Users = sequelize.define('users', {
+      login: {type: Sequelize.STRING, unique: true,allowNull: false},
+      password: {type: Sequelize.TEXT, allowNull: false},
+      id: {type: Sequelize.INTEGER, primaryKey: true,allowNull: false}
+    },{ timestamps: false });
+    
+    app.post('/login',(req,res)=>{
+      const {login,password} = req.body
+      sequelize.authenticate().then(()=>Users.findAll({
+        where:{login,password},
+        raw: true
+       })).then(d=>console.log(d))
+      
+    })
+
+
+
+
+
+
+
+
+
     const wss = new WebSocketServer({server});
     wss.on('connection', (ws) => {
       ws.on('message', (message) => {
