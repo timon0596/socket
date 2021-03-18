23bf27efca898282a71823ceb0cffd6434e440ca
vladislav <nagayychenkovladislav@gmail.com>
2021-03-18 23:58:38 +0300

    test

--- client/src/App.js
+++ client/src/App.js
@@ -14,138 +14,139 @@ import Fab from '@material-ui/core/Fab';
 import SendIcon from '@material-ui/icons/Send';
 
 const useStyles = makeStyles({
-  table: {
-    minWidth: 650,
-  },
-  chatSection: {
-    width: '100%',
-    height: '80vh'
-  },
-  headBG: {
-      backgroundColor: '#e0e0e0'
-  },
-  borderRight500: {
-      borderRight: '1px solid #e0e0e0'
-  },
-  messageArea: {
-    height: '70vh',
-    overflowY: 'auto'
-  }
+    table: {
+        minWidth: 650,
+    },
+    chatSection: {
+        width: '100%',
+        height: '80vh'
+    },
+    headBG: {
+        backgroundColor: '#e0e0e0'
+    },
+    borderRight500: {
+        borderRight: '1px solid #e0e0e0'
+    },
+    messageArea: {
+        height: '70vh',
+        overflowY: 'auto'
+    }
 });
 
 
 
 const Chat = () => {
-  const classes = useStyles();
-  const [ws] = useState(new WebSocket('ws://localhost:8080'))
-  const [msg,setMsg]=useState('')
+    const classes = useStyles();
+    const [ws] = useState(new WebSocket('ws://localhost:8080'))
+    const [msg, setMsg] = useState('')
 
-  useEffect(()=>{
-
-
-ws.addEventListener('open', function open() {
-  ws.send('something');
-});
-
-ws.addEventListener('message', function incoming(data) {
-  console.log(data.data);
-});
-
-  },[ws])
+    useEffect(() => {
 
 
-  const send = (text)=>{
-    ws.send(text)
-  }
-  return (
-      <div>
-        <Grid container>
-            <Grid item xs={12} >
-                <Typography variant="h5" className="header-message">Chat</Typography>
-            </Grid>
-        </Grid>
-        <Grid container component={Paper} className={classes.chatSection}>
-            <Grid item xs={3} className={classes.borderRight500}>
-                <List>
-                    <ListItem button key="RemySharp">
-                        <ListItemIcon>
-                        <Avatar alt="Remy Sharp" src="https://material-ui.com/static/images/avatar/1.jpg" />
-                        </ListItemIcon>
-                        <ListItemText primary="John Wick"></ListItemText>
-                    </ListItem>
-                </List>
-                <Divider />
-                <Grid item xs={12} style={{padding: '10px'}}>
-                </Grid>
-                <Divider />
-                <List>
-                    <ListItem button key="RemySharp">
-                        <ListItemIcon>
-                            <Avatar alt="Remy Sharp" src="https://material-ui.com/static/images/avatar/1.jpg" />
-                        </ListItemIcon>
-                        <ListItemText primary="Remy Sharp">Remy Sharp</ListItemText>
-                        <ListItemText secondary="online" align="right"></ListItemText>
-                    </ListItem>
-                    <ListItem button key="Alice">
-                        <ListItemIcon>
-                            <Avatar alt="Alice" src="https://material-ui.com/static/images/avatar/3.jpg" />
-                        </ListItemIcon>
-                        <ListItemText primary="Alice">Alice</ListItemText>
-                    </ListItem>
-                    <ListItem button key="CindyBaker">
-                        <ListItemIcon>
-                            <Avatar alt="Cindy Baker" src="https://material-ui.com/static/images/avatar/2.jpg" />
-                        </ListItemIcon>
-                        <ListItemText primary="Cindy Baker">Cindy Baker</ListItemText>
-                    </ListItem>
-                </List>
-            </Grid>
-            <Grid item xs={9}>
-                <List className={classes.messageArea}>
-                    <ListItem key="1">
-                        <Grid container>
-                            <Grid item xs={12}>
-                                <ListItemText align="right" primary="Hey man, What's up ?"></ListItemText>
-                            </Grid>
-                            <Grid item xs={12}>
-                                <ListItemText align="right" secondary="09:30"></ListItemText>
-                            </Grid>
-                        </Grid>
-                    </ListItem>
-                    <ListItem key="2">
-                        <Grid container>
-                            <Grid item xs={12}>
-                                <ListItemText align="left" primary="Hey, Iam Good! What about you ?"></ListItemText>
-                            </Grid>
-                            <Grid item xs={12}>
-                                <ListItemText align="left" secondary="09:31"></ListItemText>
-                            </Grid>
-                        </Grid>
-                    </ListItem>
-                    <ListItem key="3">
-                        <Grid container>
-                            <Grid item xs={12}>
-                                <ListItemText align="right" primary="Cool. i am good, let's catch up!"></ListItemText>
-                            </Grid>
-                            <Grid item xs={12}>
-                                <ListItemText align="right" secondary="10:30"></ListItemText>
-                            </Grid>
-                        </Grid>
-                    </ListItem>
-                </List>
-                <Divider />
-                <Grid container style={{padding: '20px'}}>
-                    <Grid item xs={11}>
-                        <TextField id="outlined-basic-email" label="Type Something" fullWidth onChange={e=>setMsg(e.target.value)} value={msg} />
-                    </Grid>
-                    <Grid xs={1} align="right">
-                        <Fab color="primary" aria-label="add" onClick={()=>{send(msg)}}><SendIcon /></Fab>
-                    </Grid>
+        ws.addEventListener('open', function open() {
+            ws.send('something');
+        });
+
+        ws.addEventListener('message', function incoming(data) {
+            console.log(data.data);
+            console.log(test);
+        });
+
+    }, [ws])
+
+
+    const send = (text) => {
+        ws.send(text)
+    }
+    return (
+        <div>
+            <Grid container>
+                <Grid item xs={12} >
+                    <Typography variant="h5" className="header-message">Chat</Typography>
                 </Grid>
             </Grid>
-        </Grid>
-      </div>
-  );
+            <Grid container component={Paper} className={classes.chatSection}>
+                <Grid item xs={3} className={classes.borderRight500}>
+                    <List>
+                        <ListItem button key="RemySharp">
+                            <ListItemIcon>
+                                <Avatar alt="Remy Sharp" src="https://material-ui.com/static/images/avatar/1.jpg" />
+                            </ListItemIcon>
+                            <ListItemText primary="John Wick"></ListItemText>
+                        </ListItem>
+                    </List>
+                    <Divider />
+                    <Grid item xs={12} style={{ padding: '10px' }}>
+                    </Grid>
+                    <Divider />
+                    <List>
+                        <ListItem button key="RemySharp">
+                            <ListItemIcon>
+                                <Avatar alt="Remy Sharp" src="https://material-ui.com/static/images/avatar/1.jpg" />
+                            </ListItemIcon>
+                            <ListItemText primary="Remy Sharp">Remy Sharp</ListItemText>
+                            <ListItemText secondary="online" align="right"></ListItemText>
+                        </ListItem>
+                        <ListItem button key="Alice">
+                            <ListItemIcon>
+                                <Avatar alt="Alice" src="https://material-ui.com/static/images/avatar/3.jpg" />
+                            </ListItemIcon>
+                            <ListItemText primary="Alice">Alice</ListItemText>
+                        </ListItem>
+                        <ListItem button key="CindyBaker">
+                            <ListItemIcon>
+                                <Avatar alt="Cindy Baker" src="https://material-ui.com/static/images/avatar/2.jpg" />
+                            </ListItemIcon>
+                            <ListItemText primary="Cindy Baker">Cindy Baker</ListItemText>
+                        </ListItem>
+                    </List>
+                </Grid>
+                <Grid item xs={9}>
+                    <List className={classes.messageArea}>
+                        <ListItem key="1">
+                            <Grid container>
+                                <Grid item xs={12}>
+                                    <ListItemText align="right" primary="Hey man, What's up ?"></ListItemText>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <ListItemText align="right" secondary="09:30"></ListItemText>
+                                </Grid>
+                            </Grid>
+                        </ListItem>
+                        <ListItem key="2">
+                            <Grid container>
+                                <Grid item xs={12}>
+                                    <ListItemText align="left" primary="Hey, Iam Good! What about you ?"></ListItemText>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <ListItemText align="left" secondary="09:31"></ListItemText>
+                                </Grid>
+                            </Grid>
+                        </ListItem>
+                        <ListItem key="3">
+                            <Grid container>
+                                <Grid item xs={12}>
+                                    <ListItemText align="right" primary="Cool. i am good, let's catch up!"></ListItemText>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <ListItemText align="right" secondary="10:30"></ListItemText>
+                                </Grid>
+                            </Grid>
+                        </ListItem>
+                    </List>
+                    <Divider />
+                    <Grid container style={{ padding: '20px' }}>
+                        <Grid item xs={11}>
+                            <TextField id="outlined-basic-email" label="Type Something" fullWidth onChange={e => setMsg(e.target.value)} value={msg} />
+                        </Grid>
+                        <Grid xs={1} align="right">
+                            <Fab color="primary" aria-label="add" onClick={() => { send(msg) }}><SendIcon /></Fab>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </div>
+    );
 }
 
 export default Chat; 
