e47167111b16573e1fd81c12989b5f8006723e0a
tim <timon0596@yandex.ru>
2021-03-18 21:42:03 +0300

    1

--- server/app.js
+++ server/app.js
@@ -1,11 +1,17 @@
-const WebSocket = require('ws');
+const WebSocketServer = require("ws").Server;
+    const express = require("express");
+    const http = require("http");
 
-const wss = new WebSocket.Server({ port: 8080 });
+    const app = express();
+    const server = http.createServer(app);
 
-wss.on('connection', (ws) => {
-  ws.on('message', (message) => {
-    wss.clients.forEach(client=>client.send(message))
-  });
-
-  ws.send('something');
-});
+    server.listen(8080);
+    const wss = new WebSocketServer({server});
+    wss.on('connection', (ws) => {
+      ws.on('message', (message) => {
+        wss.clients.forEach(client=>client.send(message))
+        console.log(message)
+      });
+    
+      ws.send('something');
+    });
