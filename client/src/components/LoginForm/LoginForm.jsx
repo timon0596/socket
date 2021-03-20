1b1cd7f2cc568dd8059c5085eaab58105d043a1a
vladislav <nagayychenkovladislav@gmail.com>
2021-03-20 13:56:27 +0300

    Cookies

---
+++ client/src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,143 @@
+import React, { useState } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import axios from 'axios';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>
+      {' '}
+      {new Date().getFullYear()}
+      .
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignIn() {
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+  const classes = useStyles();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Отправил', login, password);
+    // axios.post('http://localhost:8080/login', { login, password }).then((res) => { console.log(res); });
+    axios.post('https://jsonplaceholder.typicode.com/todos').then((res) => { console.log(res); });
+  };
+
+  const handleLoginInput = (e) => {
+    setLogin(e.currentTarget.value);
+    // console.log(e.currentTarget.value);
+  };
+
+  const handlePasswordInput = (e) => {
+    setPassword(e.currentTarget.value);
+    // console.log(e.currentTarget.value);
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            value={login}
+            onChange={(e) => handleLoginInput(e)}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={(e) => handlePasswordInput(e)}
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={(e) => handleSubmit(e)}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="/" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="/" variant="body2">
+                Don t have an account? Sign Up
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
--- server/app.js
+++ server/app.js
@@ -1,61 +1,67 @@
 const WebSocketServer = require("ws").Server;
-    const express = require("express");
-    const http = require("http");
-    const Sequelize = require('sequelize');
-    const cors = require('cors')
-    const bodyParser = require('body-parser')
+const express = require("express");
+const http = require("http");
+const Sequelize = require('sequelize');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
-    const app = express();
-    app.use(cors())
-    app.use(bodyParser.urlencoded({
-      extended: true
-    }));
-    app.use(bodyParser.json());
-    const server = http.createServer(app);
+const app = express();
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+app.use(cookieParser())
+const server = http.createServer(app);
 
-    server.listen(8080);
+const whitelist = ['http://localhost:3000', 'http://localhost:8080'];
+const corsOptions = {
+  origin(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not alloweb by CORS'))
+    }
+  }
+}
+app.use(cors({ credentials: true, origin: whitelist }))
+
+server.listen(8080);
+
+const sequelize = new Sequelize('socket', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql',
+  port: '3300',
+});
+
+const Users = sequelize.define('users', {
+  login: { type: Sequelize.STRING, unique: true, allowNull: false },
+  password: { type: Sequelize.TEXT, allowNull: false },
+  id: { type: Sequelize.INTEGER, primaryKey: true, allowNull: false }
+}, { timestamps: false });
+
+app.post('/login', (req, res) => {
+  const { login, password } = req.body
+  sequelize.authenticate().then(() => Users.findAll({
+    where: { login, password },
+    raw: true
+  })).then(d => console.log(d))
+})
+
+app.get('/cookie', (req, res) => {
+  // app.get('/cookie', cors(corsOptions), (req, res) => {
+  res.cookie('token', '12345ABCDE')
+  // console.log(res)
+  res.send('Set Cookie')
+})
 
 
+const wss = new WebSocketServer({ server });
+wss.on('connection', (ws) => {
+  ws.on('message', (message) => {
+    wss.clients.forEach(client => client.send(message))
+    console.log(message)
+  });
 
-
-
-    
-
-    const sequelize = new Sequelize('socket', 'root', 'root',{
-      host: 'localhost',
-      dialect: 'mysql',
-      port: '3300'
-    });
-    
-    const Users = sequelize.define('users', {
-      login: {type: Sequelize.STRING, unique: true,allowNull: false},
-      password: {type: Sequelize.TEXT, allowNull: false},
-      id: {type: Sequelize.INTEGER, primaryKey: true,allowNull: false}
-    },{ timestamps: false });
-    
-    app.post('/login',(req,res)=>{
-      const {login,password} = req.body
-      sequelize.authenticate().then(()=>Users.findAll({
-        where:{login,password},
-        raw: true
-       })).then(d=>console.log(d))
-      
-    })
-
-
-
-
-
-
-
-
-
-    const wss = new WebSocketServer({server});
-    wss.on('connection', (ws) => {
-      ws.on('message', (message) => {
-        wss.clients.forEach(client=>client.send(message))
-        console.log(message)
-      });
-    
-      ws.send('something');
-    });
+  ws.send('something');
+});
